f20ed84e82e7a7b0630af3ee1dec3e406e7bf37d
Aamir Naseer <aamirnaseer498@gmail.com>
2024-02-29 18:18:43 +0500

    Committed EndPoints Creation

---
+++ src/test/java/endpoints/EndPoints.java
@@ -0,0 +1,63 @@
+package endpoints;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class EndPoints {
+
+    public static Response createUser(User payload){
+
+        Response response= given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(payload)
+
+                .when()
+                .post(Routes.postURL);
+
+        return response;
+
+    }
+
+    public static Response getUser(String userName){
+
+        Response response= given()
+                .pathParam("username",userName)
+
+                .when()
+                .get(Routes.getURL);
+
+        return response;
+
+    }
+
+    public static Response updateUser(User payload, String userName){
+
+        Response response= given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .pathParam("username", userName)
+                .body(payload)
+
+                .when()
+                .put(Routes.putURL);
+
+        return response;
+
+    }
+
+    public static Response deleteUser(String userName){
+
+        Response response= given()
+                .pathParam("username",userName)
+
+                .when()
+                .delete(Routes.deleteURL);
+
+        return response;
+
+    }
+
+}
